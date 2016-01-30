456397df7a56edbf33c08ab16b3ba96a4018791e
Mujibur Rahman <mujibur.rahman@cention.se>
2016-01-30 21:43:37 +0800

    Add `logger` middleware to write logs to file/stdout regarding to choice
    Flag: `logToFile` will use for making choice whether it writes
    to file or std output.
    Add recovery option if there is any panic happened.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime/debug"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,23 +16,77 @@ var (
 	ginMode      *string
 	logToFile    *bool
 	useGinLogger *bool
+	logFile      = "logs/web-server.log"
 	timeFormat   = "2006-01-02T15:04:05GMT"
 )
 
 func init() {
 	port = flag.Int("port", 8000, "Http running on the port")
 	ginMode = flag.String("ginMode", "release", "Gin webframework running on release mode")
+	logToFile = flag.Bool("logToFile", true, "Log write to file")
 	useGinLogger = flag.Bool("useGinLogger", false, "Use gin logger instead of the one used in production")
+}
+
+func logger(c *gin.Context) {
+	var start time.Time
+	const logFormat = "%s " + // Timestamp
+		"%s " + // Client ip
+		"%d " + // Response code
+		"%v " + // Response Duration
+		`"%s %s %s" ` + // Request method, path and protocol
+		"%d " // Response size
+
+	defer func() {
+		const INTERNAL_SERVER_ERROR = 500
+		if err := recover(); err != nil {
+			duration := time.Now().Sub(start)
+			log.Printf(logFormat+"\n%v\n%s",
+				start.Format(timeFormat),
+				c.ClientIP(),
+				INTERNAL_SERVER_ERROR,
+				duration,
+				c.Request.Method,
+				c.Request.URL.Path,
+				c.Request.Proto,
+				c.Writer.Size(),
+				err,
+				debug.Stack(),
+			)
+			c.AbortWithStatus(INTERNAL_SERVER_ERROR)
+		}
+	}()
+	start = time.Now()
+	c.Next()
+	duration := time.Now().Sub(start)
+	log.Printf(logFormat+"\n%s",
+		start.Format(timeFormat),
+		c.ClientIP(),
+		c.Writer.Status(),
+		duration,
+		c.Request.Method,
+		c.Request.URL.Path,
+		c.Request.Proto,
+		c.Writer.Size(),
+		c.Errors.String(),
+	)
 }
 
 func main() {
 	flag.Parse()
+	if *logToFile {
+		logWriteTo, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Printf("error opening to log file: %v", err)
+		}
+		log.SetOutput(logWriteTo)
+	}
 	gin.SetMode(*ginMode)
 	var route *gin.Engine
 	if *useGinLogger {
 		route = gin.Default()
 	} else {
 		route = gin.New()
+		route.Use(logger)
 	}
 
 	route.GET("", func(ctx *gin.Context) {
